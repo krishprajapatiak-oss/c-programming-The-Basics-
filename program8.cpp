7091832febf3e380714470eb2f19f14a9cc31f79
Krish Prajapati <krish.prajapati.ak@gmail.com>
2026-09-22 17:27:35 +0530

    Add files via upload

---
+++ program8.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	
+	char n;
+	
+		printf("enter any character:");
+		scanf("%c",&n);
+		
+		printf("Character = %c",n);
+		
+	return(0);
+	
+} 
---
+++ program9.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+	
+	char n;
+		printf("Enter your character:");
+		scanf("%c",&n);
+		
+		printf("ASCII value for character '%c' is %d",n,n);
+	return(0);
+}
